c8a191cc2f54b3decd2d14eddf03122125339e46
Luis Ferreira <apenaspara1998@gmail.com>
2020-05-13 15:45:52 +0100

    Event Capture Zone. Allows to define a zone with a certain scale in the screen from which mouse event will be captured and converted to said scale

--- game/game.js
+++ game/game.js
@@ -1,9 +1,17 @@
 MH.scope(() => {
 
+    const {MouseCaptureZone, EventEmitter} = MH.module("engine.events");
+    const {gameContainer} = MH.module("engine.html");
+    console.log(MouseCaptureZone);
+    console.log(EventEmitter);
 
+    const emitter = new EventEmitter();
+    emitter.listen("mousedown", console.log);
+    emitter.listen("mouseup", console.log);
+    //emitter.listen("mousemove", console.log);
 
-    console.log(MH.module("engine"));
-
-
+    document.body.appendChild(gameContainer);
+    window.emitter = emitter;
+    window.MouseCaptureZone = MouseCaptureZone;
 
 });
