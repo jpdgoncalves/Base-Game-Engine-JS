7f83a9855dbf0a42195e58326d2e77662aa2fcac
Luis Ferreira <apenaspara1998@gmail.com>
2020-05-14 15:37:27 +0100

    Refactoring of code. The way the inputs were being handled seemed completly innadequate

--- game/game.js
+++ game/game.js
@@ -1,17 +1,3 @@
 MH.scope(() => {
-
-    const {MouseCaptureZone, EventEmitter} = MH.module("engine.events");
-    const {gameContainer} = MH.module("engine.html");
-    console.log(MouseCaptureZone);
-    console.log(EventEmitter);
-
-    const emitter = new EventEmitter();
-    emitter.listen("mousedown", console.log);
-    emitter.listen("mouseup", console.log);
-    //emitter.listen("mousemove", console.log);
-
-    document.body.appendChild(gameContainer);
-    window.emitter = emitter;
-    window.MouseCaptureZone = MouseCaptureZone;
-
+    
 });
