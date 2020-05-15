843c208137ec6dce973befe265022156abd35f9c
Luis Ferreira <apenaspara1998@gmail.com>
2020-05-15 11:52:55 +0100

    A few aditions to the base of the game engine

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Game-Engine-JS
+# Base Game Engine JS
+This is a base project which is intended to be used to make 2D Game engines. Or acttually make a game.
--- game/game.js
+++ game/game.js
@@ -1,8 +1,5 @@
 MH.scope(() => {
-    const {Timer} = MH.module("engine.core");
 
-    const timer = new Timer(30);
-    timer.callback = console.log;
-    
-    window.timer = timer;
+    const game = MH.create("game");
+
 });
